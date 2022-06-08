47d5db6aff98689b767192e6509c69a9dad7de41
MOOSCLES <107116890+MOOSCLES@users.noreply.github.com>
2022-06-08 14:48:48 +0300

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ a = 1
 while b < 50:
     b = a + b
     basic.show_number(b)
+if b == 50:
+    print("limit numper")
+    print(b)
--- main.ts
+++ main.ts
@@ -4,3 +4,8 @@ while (b < 50) {
     b = a + b
     basic.showNumber(b)
 }
+if (b == 50) {
+    console.log("limit numper")
+    console.log(b)
+}
+
